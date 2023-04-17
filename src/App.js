319aa70dd38f718533f0ae8fd07a7cd1297e04fe
HenryXanderTalent <henry.opara@xandertalent.com>
2023-04-17 13:12:03 +0100

    added code for jsx version

--- src/App.js
+++ src/App.js
@@ -22,7 +22,33 @@ const container = document.getElementById("root");
 const root = ReactDOM.createRoot(container);
 root.render(React.createElement(App));
 
+//JSX version4
 
+// import React from "react";
+// import "./styles.css";
+
+// function Pet(props) {
+//   return (
+//     <div>
+//       <h1>{props.name}</h1>
+//       <h2>{props.animal}</h2>
+//       <h3>{props.breed}</h3>
+//     </div>
+//   );
+// }
+
+// function App() {
+//   return (
+//     <div>
+//       <h1>Adopt Me!</h1>
+//       <Pet name="Luna" animal="Dog" breed="Havanese" />
+//       <Pet name="Pepper" animal="Bird" breed="Cockatiel" />
+//       <Pet name="Doink" animal="Cat" breed="Mix" />
+//     </div>
+//   );
+// }
+
+// export default App;
 
 
       
